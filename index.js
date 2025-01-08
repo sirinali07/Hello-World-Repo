50c5df18f44efb26c154d155d106c333590968ae
Sirin Ali <118505493+sirinali07@users.noreply.github.com>
2025-01-08 15:49:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ const port = process.env.PORT || 8080;
 const express = require('express');
 const app = express();
 
-// Use the 'chalk' library for adding colors to the text
-const chalk = require('chalk');
-
 app.get('/', (req, res) => {
   const currentDate = new Date();
   const dateTime = currentDate.toLocaleString();
-  
+
   res.send(`
     <div style="font-family: Arial, sans-serif; text-align: center; padding: 50px;">
-      <h1 style="color: ${chalk.blue('dodgerblue')}; font-size: 50px;">
-        ${chalk.green('Hello World!')}
+      <h1 style="color: dodgerblue; font-size: 50px;">
+        Hello World!
       </h1>
-      <p style="color: ${chalk.yellow('gold')}; font-size: 20px;">
+      <p style="color: gold; font-size: 20px;">
         Current Date and Time: ${dateTime}
       </p>
     </div>
