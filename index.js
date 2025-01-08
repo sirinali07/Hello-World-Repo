d78c71197e8923d15b53164792dc13be67d7ba00
Sirin Ali <118505493+sirinali07@users.noreply.github.com>
2025-01-08 14:47:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const port = process.env.PORT || 8080;
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!.!'))
+app.get('/', (req, res) => res.send('Sirin-Hello World!.!'))
 
 app.get('/readiness', (req, res) => res.send('Ready !!'))
 
